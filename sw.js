34488dd1029fcae55e6d2de20100a662f64ecad7
hitesh1210 <hiteshkadu137@gmail.com>
2023-07-22 20:43:08 -0400

    Updated the Host Header Injection Blog

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230722.230035';
+const cacheName = 'chirpy-20230723.061143';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
